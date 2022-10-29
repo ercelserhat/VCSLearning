f449448a4f7e76c73020d8c4e7d531820cd10c9f
ercelserhat <ercelserhat@gmail.com>
2022-10-30 01:39:59 +0300

    Main Test Commit

--- app/src/main/java/com/ercelserhat/vsclearning/MainActivity.kt
+++ app/src/main/java/com/ercelserhat/vsclearning/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("First Commit")
         println("First Branch - First Commit")
+        println("Main Test Commit")
     }
 
 
